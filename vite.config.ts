4accf2cf1e1df8a949d2c1e3370221954abdbd26
Your Name <you@example.com>
2023-05-24 10:23:33 -0600

    pruebas para correr en local

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit()]
 };
 
 export default config;
+
+{
+  "extends": "./tsconfig.json",
+  "compilerOptions": {
+    "allowJs": true,
+    "checkJs": true,
+    "esModuleInterop": true,
+    "forceConsistentCasingInFileNames": true,
+    "resolveJsonModule": true,
+    "skipLibCheck": true,
+    "sourceMap": true,
+    "strict": true
+  }
+}
